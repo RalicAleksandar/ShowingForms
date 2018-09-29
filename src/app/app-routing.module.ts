bef10189afab39bcb315feef2e7e6d774654cbef
Ralic <acaralic@gmail.com>
2018-09-30 01:01:08 +0200

    +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
 	{
+		path: '',
+		loadChildren: './forms/forms.module#FormsModule',
+	},
+	{
 		path: 'login',
 		loadChildren: './login/login.module#LoginModule',
 	},
